b8325fe6a8d70e99c89bfb83ce358462bd4b4ed5
luisadolfojimenezlopez <luis.jimenezlopez7@tecnacional.edu.ni>
2025-09-23 10:46:52 -0600

    Feat:"I have created the full view of the visitor table."

--- resources/views/visitors/index.blade.php
+++ resources/views/visitors/index.blade.php
@@ -1,3 +1,34 @@
-<div>
-    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
-</div>
+@extends('layouts.panel')
+
+@section('title', 'Visitor/Crear')
+
+@section('content')
+
+    <div class="col-xl-12 order-xl-1">
+        <div class="card bg-secondary shadow">
+            <div class="card-header bg-white border-0">
+                <div class="row align-items-center">
+                    <div class="col-8">
+                        <h3 class="mb-0">
+                            <i class="fas fa-plus-circle"></i> Registrar Visitor</h3>
+                        </div>
+                        <div class="col-4 text-right">
+                            <a href="{{ route('visitors.index') }}" class="btn btn-sm btn-primary">
+                                <i class="fas fa-arrow-left"></i> Volver
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-body container-fluid">
+
+                    <form action="{{ route('visitors.store') }}" method="POST">
+                        @csrf
+
+                        @include('visitors.form')
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
