ef570ea54938930e025f53ba72cf7802b5047b01
jenn21-art <jenn21-art>
2025-09-24 07:47:05 -0600

    feat: "hotels migration creation"

--- database/migrations/2025_09_23_171403_create_hotels_table.php
+++ database/migrations/2025_09_23_171403_create_hotels_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migration.
+     * Run the migrations.
      */
     public function up(): void
     {
