03ac7b68692b8bc2128e78646d3fd5d56854e189
jenn21-art <jenn21-art>
2025-09-25 08:18:13 -0600

    feat: creating a form view of guides

--- resources/views/guides/form.blade.php
+++ resources/views/guides/form.blade.php
@@ -2,9 +2,9 @@
     <div class="row">
         <div class="col-lg-6">
             <div class="form-group">
-                <label class="form-control-label" for="name">Nombre completo</label>
+                <label class="form-control-label" for="name">Nombre del guia</label>
                 <input type="text" id="name" name="name" class="form-control form-control-alternative"
-                    placeholder="Ingresar nombre del estudiante" value="{{ old('name', $students->name ?? '') }}">
+                    placeholder="Ingresar nombre del guia" value="{{ old('name', $guides->name ?? '') }}">
             </div>
         </div>
     </div>
@@ -12,10 +12,10 @@
     <div class="row">
         <div class="col-lg-6">
             <div class="form-group">
-                <label class="form-control-label" for="code_student">Código del Estudiante</label>
-                <input type="number" id="code_student" name="code_student"
-                    class="form-control form-control-alternative" placeholder="Ingresar código del estudiante"
-                    value="{{ old('code_student', $students->code_student ?? '') }}">
+                <label class="form-control-label" for="email">correo elctronico del guia</label>
+                <input type="text" id="email" name="email"
+                    class="form-control form-control-alternative" placeholder="Ingresar correo electronico "
+                    value="{{ old('email', $guides->email ?? '') }}">
             </div>
         </div>
     </div>
@@ -23,10 +23,10 @@
     <div class="row">
         <div class="col-lg-6">
             <div class="form-group">
-                <label class="form-control-label" for="identification">Número de cédula</label>
-                <input type="text" id="identification" name="identification"
-                    class="form-control form-control-alternative" placeholder="Ingresar número de cédula"
-                    value="{{ old('identification', $students->identification ?? '') }}">
+                <label class="form-control-label" for="identification">Número de telefono</label>
+                <input type="text" id="telephone" name="telephone"
+                    class="form-control form-control-alternative" placeholder="Ingresar número de telefono"
+                    value="{{ old('telephone', $guides->telephone ?? '') }}">
             </div>
         </div>
     </div>
@@ -34,9 +34,29 @@
     <div class="row">
         <div class="col-lg-6">
             <div class="form-group">
-                <label class="form-control-label" for="age">Edad</label>
+                <label class="form-control-label" for="experience">Experiencia</label>
+                <input type="experience" id="experience" name="experience" class="form-control form-control-alternative"
+                    placeholder="Ingresar experiencia" value="{{ old('experience', $guides->experience ?? '') }}">
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="form-group">
+                <label class="form-control-label" for="language">Fecha de Nacimiento</label>
+                <input type="text" id="language" name="language" class="form-control form-control-alternative"
+                    value="{{ old('language', $guides->language ?? '') }}">
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="form-group">
+                <label class="form-control-label" for="age">Fecha de nacimiento</label>
                 <input type="number" id="age" name="age" class="form-control form-control-alternative"
-                    placeholder="Ingresar edad" value="{{ old('age', $students->age ?? '') }}">
+                    placeholder="Ingresar fecha de nacimiento" value="{{ old('age', $guides->age ?? '') }}">
             </div>
         </div>
     </div>
@@ -44,176 +64,15 @@
     <div class="row">
         <div class="col-lg-6">
             <div class="form-group">
-                <label class="form-control-label" for="d_birth">Fecha de Nacimiento</label>
-                <input type="date" id="d_birth" name="d_birth" class="form-control form-control-alternative"
-                    value="{{ old('d_birth', $students->d_birth ?? '') }}">
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-lg-6">
-            <div class="form-group">
-                <label class="form-control-label" for="p_birth">Lugar de Nacimiento</label>
-                <input type="text" id="p_birth" name="p_birth" class="form-control form-control-alternative"
-                    placeholder="Ingresa lugar de nacimiento" value="{{ old('p_birth', $students->p_birth ?? '') }}">
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-lg-6">
-            <div class="form-group">
-                <label class="form-control-label" for="address">Dirección</label>
-                <input type="text" id="address" name="address" class="form-control form-control-alternative"
-                    placeholder="Ingresa su dirección" value="{{ old('address', $students->address ?? '') }}">
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-lg-6">
-            <div class="form-group">
-                <label for="sex">Género</label>
-                <select name="sex" id="sex" class="form-control">
+                <label for="gender">Género</label>
+                <select name="gender" id="sex" class="form-control">
                     <option value="" disabled>Seleccione su género</option>
-                    <option value="Masculino" {{ old('sex', $students->sex ?? '') == 'Masculino' ? 'selected' : '' }}>Masculino</option>
-                    <option value="Femenino" {{ old('sex', $students->sex ?? '') == 'Femenino' ? 'selected' : '' }}>Femenino</option>
+                    <option value="Masculino" {{ old('gender', $guides->gender ?? '') == 'Masculino' ? 'selected' : '' }}>Masculino</option>
+                    <option value="Femenino" {{ old('gender', $guides->gender ?? '') == 'Femenino' ? 'selected' : '' }}>Femenino</option>
                 </select>
             </div>
         </div>
     </div>
-
-    <div class="row">
-        <div class="col-lg-6">
-            <div class="form-group">
-                <label for="nationality">Nacionalidad</label>
-                <select name="nationality" id="nationality" class="form-control">
-                    <option value="" disabled>Seleccione su nacionalidad</option>
-                    <option value="Nicaragüense" {{ old('nationality', $students->nationality ?? '') == 'Nicaragüense' ? 'selected' : '' }}>Nicaragüense</option>
-                    <option value="Costarricense" {{ old('nationality', $students->nationality ?? '') == 'Costarricense' ? 'selected' : '' }}>Costarricense</option>
-                    <option value="Hondureño" {{ old('nationality', $students->nationality ?? '') == 'Hondureño' ? 'selected' : '' }}>Hondureño/a</option>
-                    <option value="Salvadoreño" {{ old('nationality', $students->nationality ?? '') == 'Salvadoreño' ? 'selected' : '' }}>Salvadoreño/a</option>
-                </select>
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-lg-6">
-            <div class="form-group">
-                <label for="ethnicity">Etnia</label>
-                <select name="ethnicity" id="ethnicity" class="form-control">
-                    <option value="" disabled>Seleccione su etnia</option>
-                    <option value="Mestizo" {{ old('ethnicity', $students->ethnicity ?? '') == 'Mestizo' ? 'selected' : '' }}>Mestizo</option>
-                    <option value="Ruso" {{ old('ethnicity', $students->ethnicity ?? '') == 'Ruso' ? 'selected' : '' }}>Ruso</option>
-                    <option value="Criollo" {{ old('ethnicity', $students->ethnicity ?? '') == 'Criollo' ? 'selected' : '' }}>Criollo</option>
-                </select>
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-lg-6">
-            <div class="form-group">
-                <label class="form-control-label" for="telephone">Número Telefónico</label>
-                <input type="text" id="telephone" name="telephone" class="form-control form-control-alternative"
-                    placeholder="Ingresar su número telefónico" value="{{ old('telephone', $students->telephone ?? '') }}">
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-lg-6">
-            <div class="form-group">
-                <label for="country">País</label>
-                <select name="country" id="country" class="form-control">
-                    <option value="" disabled>Seleccione su país de procedencia</option>
-                    <option value="Nicaragua" {{ old('country', $students->country ?? '') == 'Nicaragua' ? 'selected' : '' }}>Nicaragua</option>
-                    <option value="Costa Rica" {{ old('country', $students->country ?? '') == 'Costa Rica' ? 'selected' : '' }}>Costa Rica</option>
-                    <option value="El Salvador" {{ old('country', $students->country ?? '') == 'El Salvador' ? 'selected' : '' }}>El Salvador</option>
-                </select>
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-lg-6">
-            <div class="form-group">
-                <label class="form-control-label" for="mail">Correo electrónico</label>
-                <input type="email" id="mail" name="mail" class="form-control form-control-alternative"
-                    placeholder="Ingrese su Correo electrónico" value="{{ old('mail', $students->mail ?? '') }}">
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-lg-6">
-            <div class="form-group">
-                <label for="departament">Departamento</label>
-                <select name="departament" id="departament" class="form-control">
-                    <option value="" disabled>Seleccione su departamento</option>
-                    <option value="Estelí" {{ old('departament', $students->departament ?? '') == 'Estelí' ? 'selected' : '' }}>Estelí</option>
-                    <option value="Chontales" {{ old('departament', $students->departament ?? '') == 'Chontales' ? 'selected' : '' }}>Chontales</option>
-                    <option value="Managua" {{ old('departament', $students->departament ?? '') == 'Managua' ? 'selected' : '' }}>Managua</option>
-                    <option value="Nueva Segovia" {{ old('departament', $students->departament ?? '') == 'Nueva Segovia' ? 'selected' : '' }}>Nueva Segovia</option>
-                </select>
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-lg-6">
-            <div class="form-group">
-                <label class="form-control-label" for="municipality">Municipio</label>
-                <input type="text" id="municipality" name="municipality"
-                    class="form-control form-control-alternative" placeholder="Ingrese su municipio"
-                    value="{{ old('municipality', $students->municipality ?? '') }}">
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-lg-6">
-            <div class="form-group">
-                <label for="academic_level">Nivel Académico</label>
-                <select name="academic_level" id="academic_level" class="form-control">
-                    <option value="" disabled>Seleccione su nivel académico</option>
-                    <option value="Primaria" {{ old('academic_level', $students->academic_level ?? '') == 'Primaria' ? 'selected' : '' }}>Primaria</option>
-                    <option value="Bachiller" {{ old('academic_level', $students->academic_level ?? '') == 'Bachiller' ? 'selected' : '' }}>Bachiller</option>
-                    <option value="Licenciado" {{ old('academic_level', $students->academic_level ?? '') == 'Licenciado' ? 'selected' : '' }}>Licenciado</option>
-                    <option value="Ingeniero" {{ old('academic_level', $students->academic_level ?? '') == 'Ingeniero' ? 'selected' : '' }}>Ingeniero</option>
-                    <option value="Técnico" {{ old('academic_level', $students->academic_level ?? '') == 'Técnico' ? 'selected' : '' }}>Técnico</option>
-                </select>
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-lg-6">
-            <div class="form-group">
-                <label for="marital_status">Estado Civil</label>
-                <select name="marital_status" id="marital_status" class="form-control">
-                    <option value="" disabled>Seleccione el estado civil</option>
-                    <option value="Soltero/a" {{ old('marital_status', $students->marital_status ?? '') == 'Soltero/a' ? 'selected' : '' }}>Soltero/a</option>
-                    <option value="Casado/a" {{ old('marital_status', $students->marital_status ?? '') == 'Casado/a' ? 'selected' : '' }}>Casado/a</option>
-                    <option value="Divorciado/a" {{ old('marital_status', $students->marital_status ?? '') == 'Divorciado/a' ? 'selected' : '' }}>Divorciado/a</option>
-                    <option value="Viudo/a" {{ old('marital_status', $students->marital_status ?? '') == 'Viudo/a' ? 'selected' : '' }}>Viudo/a</option>
-                </select>
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-lg-6">
-            <div class="form-group">
-                <label class="form-control-label" for="responsible">Responsable</label>
-                <input type="text" id="responsible" name="responsible" class="form-control form-control-alternative"
-                    placeholder="Ingresar responsable del estudiante" value="{{ old('responsible', $students->responsible ?? '') }}">
-            </div>
-        </div>
-    </div>
-</div>
 
 <hr class="my-4" />
 
@@ -222,7 +81,7 @@
 <div class="pl-lg-4">
     <div class="form-group">
         <button type="submit" class="btn btn-primary">
-            <i class="fas fa-save"></i> Guardar Estudiante
+            <i class="fas fa-save"></i> Guardar Guia
         </button>
     </div>
 </div>
