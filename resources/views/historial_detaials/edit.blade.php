22507a0a588fc754b71f1d4bc1074c978ef6f93b
Ulises Mauricio <ulises.castellon1@tecnacional.edu.ni>
2025-09-25 07:36:24 -0600

    feed: create historial_detaials_views: edit

--- resources/views/historial_detaials/edit.blade.php
+++ resources/views/historial_detaials/edit.blade.php
@@ -1,3 +1,35 @@
-<div>
-    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
-</div>
+@extends('layouts.panel')
+
+@section('title', 'historial/Actualizar')
+
+@section('content')
+
+    <div class="col-x1-12 order-x1-1">
+        <div class="card bg-secondary shadow">
+            <div class="card-header bg-white border-0">
+                <div class="row align-items-center">
+                    <div class="col-8">
+                        <h3 class="mb-0">
+                            <i class="fas fa-plus-circle"x</i> Actualizar historial
+                        </h3>
+
+                    </div>
+                    <div class="col-4 text-right">
+                        <a href="ff route('historial_detaials.index') ))" class="btn btn-sm btn-primary">
+                            <i class="fas fa-arrow- left"></i> Volver
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-body container-fluid">
+                <form action="f/ route('historial_detaials.update', $historial_detaials->$id) 1" method="POST">
+                    @csrf
+                    @method('PUT')
+                    @include('historial_detaials.form')
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
