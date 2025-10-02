510f51564bf38fd5a41640ce5476b4494ee6501a
Ulises-mauricio <ulises.castellon1@tecnacional.edu.ni>
2025-10-02 07:42:08 -0600

    feat: changes in request feat: changes in controller feat: changes in views feat: changes in models

--- app/Http/Controllers/Tour_categorieController.php
+++ app/Http/Controllers/Tour_categorieController.php
@@ -2,51 +2,51 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\TourCategory;
-use App\Http\Requests\TourCategoryRequest;
+use App\Models\Tour_categorie;
+use App\Http\Requests\Tour_categorieRequest;
 
-class TourCategoryController extends Controller
+class Tour_categorieController extends Controller
 {
     public function index()
     {
-        $tours_categories = TourCategory::latest()->paginate(10);
+        $tours_categories = Tour_categorie::latest()->paginate(10);
         return view('tours_categories.index', compact('tours_categories'));
     }
 
     public function create()
     {
-        $tours_categories = new TourCategory();
+        $tours_categories = new Tour_categorie();
         return view('tours_categories.create', compact('tours_categories'));
     }
 
-    public function store(TourCategoryRequest $request)
+    public function store(Tour_categorieRequest $request)
     {
-        TourCategory::create($request->validated());
+        Tour_categorie::create($request->validated());
         return redirect()->route('tours_categories.index')->with('success', 'Categoría de Recorrido creada con éxito');
     }
 
     public function show(string $id)
     {
-        $tours_categories = TourCategory::find($id);
+        $tours_categories = Tour_categorie::find($id);
         return view('tours_categories.show', compact('tours_categories'));
     }
 
     public function edit(int $id)
     {
-        $tours_categories = TourCategory::find($id);
+        $tours_categories = Tour_categorie::find($id);
         return view('tours_categories.edit', compact('tours_categories'));
     }
 
-    public function update(TourCategoryRequest $request, int $id)
+    public function update(Tour_categorieRequest $request, int $id)
     {
-        $tours_categories = TourCategory::find($id);
+        $tours_categories = Tour_categorie::find($id);
         $tours_categories->update($request->validated());
         return redirect()->route('tours_categories.index')->with('updated', 'Categoría de tour actualizada con éxito');
     }
 
     public function destroy(int $id)
     {
-        $tours_categories = TourCategory::find($id);
+        $tours_categories = Tour_categorie::find($id);
         $tours_categories->delete();
         return redirect()->route('tours_categories.index')->with('deleted', 'Categoría de tour eliminada correctamente');
     }
